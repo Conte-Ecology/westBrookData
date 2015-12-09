a24751dd347085a199e1ad62da0d42cbe9db338d
Evan Childress <evan@osensei.cns.umass.edu>
2015-12-09 13:30:29 -0500

    removing makeDatabase scripts from the main directory

--- data_table_stage/form_data_locations.R
+++
@@ -1,9 +0,0 @@
-source_data <- readRDS(
-	file=file.path(original_data_dir,'section_table.rds')
-)
-
-dbWriteTable(link$conn, 'data_locations', source_data, row.names=FALSE,
-						 overwrite=TRUE, append=FALSE)
-
-
-
