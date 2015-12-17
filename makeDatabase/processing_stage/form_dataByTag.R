b2bb05095a679677944c0f77f6e017e829c64560
Evan Childress <evan.childress@gmail.com>
2015-12-17 16:45:47 -0500

    cohort assignment close, everything else functional

--- makeDatabase/processing_stage/form_dataByTag.R
+++ makeDatabase/processing_stage/form_dataByTag.R
@@ -4,7 +4,9 @@ captureQuery<-paste("SELECT tag,species,sample_name,cohort,observed_length",
 captures<-data.table(dbGetQuery(con,captureQuery))
 setkey(captures,tag)
 
-#functions to get info by tag
+
+
+#functions and reference data to get info by tag
 getSpecies<-function(species,tag){
   species<-unique(na.omit(species))
   if(length(species==1)){return(species)}
@@ -19,12 +21,41 @@ getMinLength<-function(x){
   if(m!=Inf) return(m) else return(as.numeric(NA))
 }
 
+cohortBins<-data.table(dbGetQuery(con,"SELECT * FROM data_yoy_bins"))
+
+getCohort<-function(cohort,species,length,sample){
+  execEnv<-env()
+  species<-unique(na.omit(species))
+  if(length(species)>1) stop("cannot assign cohort for multiple species tag")
+  if(length(species==0)) return(NA)
+  cohort<-unique(na.omit(cohort))
+  if(length(cohort==1)) return(cohort)
+  if(length(cohort==0)){
+    minLength<-suppressWarnings(min(length,na.rm=T))
+    if(minLength==Inf) return(NA)
+    sample<-sample[which(length==minLength)]
+    bins<-cohortBins[species==get('species',envir=execEnv)&
+                          sample_name==get('sample',envir=execEnv),
+                          list(cohort_min_length,
+                               cohort_max_length,
+                               cohort)]
+    cohort<-bins$cohort[intersect(which(minLength>bins$cohort_min_length),
+                                  which(minLength<bins$cohort_max_length))]
+    return(cohort)
+  }
+  if(length(cohort>1)){
+    whichHaveCohort<-which(!is.na(cohort))
+    #return the cohort call made first if multiple were made
+    return(cohort[which.min(sample[whichHaveCohort])])
+  }
+}
+
 #get data from from seasonal sampling by tag
 dataByTag<-captures[,list(species=getSpecies(species),
                           minLength=getMinLength(observed_length),
-                          minLengthDate=detection_date[which.min(observed_length)],
-                          firstSampleObserved=min(sample_name),
-                          lastSampleObserved=max(sample_name)
+                          minLengthSample=detection_date[which.min(observed_length)],
+                          firstCaptureSample=min(sample_name),
+                          lastCaptureSample=max(sample_name)
                           ),by=tag]
 
 #add in lastAntennaDetection from antenna data
@@ -45,6 +76,8 @@ dead<-data.table(dbGetQuery(con,"SELECT * FROM tags_dead"))
 setkey(dead,tag)
 
 dataByTag<-dataByTag[dead]
+
+
 
 dbDropTable("dataByTag")
 dbWriteTable(con, 'dataByTag', dataByTag, row.names=FALSE)
