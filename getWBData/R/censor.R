f7934b54e8b57f8992a8ac2ca53d86e779808940
Evan Childress <evan.childress@gmail.com>
2015-12-31 08:53:53 -0500

    changed file permissions and added censor() prior to first capture

--- getWBData/R/censor.R
+++ getWBData/R/censor.R
@@ -6,9 +6,10 @@
 #'@param dead Logical indicating whether to censore individuals that are known dead (see also createKnownState)
 #'@returns A data.frame without the rows
 #'
-censor<-function(cmrData,emigrated=T,dead=F){
+censor<-function(cmrData,emigrated=T,dead=F,beforeObs=T){
   reconnect()
   
+
   columns<-"tag"
   notNull<-NULL
   if(dead){columns<-c(columns,"date_known_dead")
@@ -17,30 +18,43 @@ censor<-function(cmrData,emigrated=T,dead=F){
   if(emigrated){columns<-c(columns,"date_emigrated")
                 notNull<-c(notNull,"date_emigrated IS NOT NULL")
   }
-  query<-paste("SELECT",
-               paste(columns,collapse=","),
-               "FROM data_by_tag",
-               "WHERE",
-               paste(notNull,collapse=" OR "))
+  if(emigrated|dead){ #censor emigrated and/or dead
+    query<-paste("SELECT",
+                 paste(columns,collapse=","),
+                 "FROM data_by_tag",
+                 "WHERE",
+                 paste(notNull,collapse=" OR "))
+    
+    toCensor<-dbGetQuery(con,query)
+    dateNames<-(names(toCensor)[2:ncol(toCensor)])
+    expr<-paste0("as.POSIXct(min(",paste(dateNames,collapse=","),",na.rm=T))")
+    toCensor<-toCensor %>% group_by(tag) %>% transmute_(censorDate=expr)
+    
+    samples<-dbGetQuery(con,"SELECT sample_name,start_date FROM data_seasonal_sampling")
+    firstCensoredSample<-function(date){
+      samples %>% 
+        filter(start_date>date) %>% 
+          filter(start_date==min(start_date)) %>% 
+            .[["sample_name"]]
+    }
+    toCensor<-toCensor %>% 
+                group_by(tag) %>% 
+                  mutate(firstCensoredSample=firstCensoredSample(censorDate))
+    
+    cmrData<-toCensor %>%
+               select(tag,firstCensoredSample) %>%
+                 right_join(cmrData,by="tag") %>%
+                   filter(sampleNumber<firstCensoredSample|is.na(firstCensoredSample)) %>%
+                     select(-firstCensoredSample)
+  }#end emigrated or dead section
   
-  toCensor<-dbGetQuery(con,query)
-  dateNames<-(names(toCensor)[2:ncol(toCensor)])
-  expr<-paste0("as.POSIXct(min(",paste(dateNames,collapse=","),",na.rm=T))")
-  toCensor<-toCensor %>% group_by(tag) %>% transmute_(censorDate=expr)
-  
-  samples<-dbGetQuery(con,"SELECT sample_name,start_date FROM data_seasonal_sampling")
-  firstCensoredSample<-function(date){
-    samples %>% 
-      filter(start_date>date) %>% 
-        filter(start_date==min(start_date)) %>% 
-          .[["sample_name"]]
+  if(beforeObs){ #remove occasions prior to the first capture
+    cmrData<-cmrData %>%
+               group_by(tag) %>%
+                 mutate(firstObs=sampleNumber[min(which(enc==1))]) %>%
+                   filter(sampleNumber>=firstObs) %>%
+                     select(-firstObs)
   }
-  toCensor<-toCensor %>% 
-              group_by(tag) %>% 
-                mutate(firstCensoredSample=firstCensoredSample(censorDate))
   
-  
-  #need to actually censor the data, but they don't exist yet
-  
-  return(censoredData)
+  return(cmrData)
 }
