ed3c193bd58b364d30dad7644e421129fdaf13d5
Evan Childress <evan.childress@gmail.com>
2016-01-29 09:00:45 -0500

    adding sample/river combos where proportion_sampled==0 to data_seasonal_sampling

--- makeDatabase/data_table_stage/form_data_seasonal_sampling.R
+++ makeDatabase/data_table_stage/form_data_seasonal_sampling.R
@@ -24,10 +24,37 @@ seasonalSampling[year==2002&season==4&river=="west brook",proportion_sampled:=2/
 seasonalSampling[river == 'west brook' & year == 2003 & season == 4, proportion_sampled:=30/47 ] 
 seasonalSampling[ river == 'west brook' & year == 2004 & season == 4, proportion_sampled:=3/47 ]
 
-seasonalSampling[ river == 'west brook' & year == 2005 & season == 4, proportion_sampled:=0]
-seasonalSampling[ river == 'west brook' & year == 2007 & season == 4, proportion_sampled:=0 ]
-seasonalSampling[year==2002 & season==4 & river!="west brook",proportion_sampled:=0]
-
+fillProp0<-function(riv,y,seas){
+  if(nrow(seasonalSampling[year==y&season==seas])>0&
+     nrow(seasonalSampling[year==y&season==seas&river==riv])==0){
+  newRow<-seasonalSampling[year==y&season==seas,
+                            .(sample_name=unique(sample_name),
+                              river=riv,
+                              median_date=unique(median_date),
+                              start_date=unique(median_date),
+                              end_date=unique(median_date),
+                              order=unique(order),
+                              seasonal=unique(seasonal),
+                              start_julian_day=as.numeric(format(unique(median_date),"%j")),
+                              end_julian_day=as.numeric(format(unique(median_date),"%j")),
+                              year=y,
+                              sample_number=unique(sample_number),
+                              season=seas,
+                              proportion_sampled=0)]
+  seasonalSampling<<-rbind(seasonalSampling,newRow)
+  }
+}
+propsToFill<-data.table(river=c("west brook","west brook","wb jimmy","wb obear","wb mitchell"),
+                        year= c( 2005,        2007,       2002,       2002,      2002),
+                        season=c(4,           4,          4,          4,         4))
+for(i in 1:nrow(propsToFill)){
+  fillProp0(riv=propsToFill$river[i],
+            y=propsToFill$year[i],
+            seas=propsToFill$season[i])
+}
+# seasonalSampling[ river == 'west brook' & year == 2005 & season == 4, proportion_sampled:=0]
+# seasonalSampling[ river == 'west brook' & year == 2007 & season == 4, proportion_sampled:=0 ]
+# seasonalSampling[year==2002 & season==4 & river!="west brook",proportion_sampled:=0]
 
 dbDropTable("data_seasonal_sampling")
 dbWriteTable(con,name="data_seasonal_sampling",value=seasonalSampling,row.names=FALSE)
