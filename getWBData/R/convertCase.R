4541702015a23ef0162313e3b99dc25a178c08aa
evanchildress <evan.childress@gmail.com>
2016-02-25 11:51:29 -0500

    rewriting convertCase functions to vectorize while maintaining the appropriate namespace

--- getWBData/R/convertCase.R
+++ getWBData/R/convertCase.R
@@ -1,26 +1,28 @@
 #'Convert camel-case string to underscore separated
 #'
-#'@param x Character (vector) to be converted to camel-case
-#'@return A string (vector) with underscore separated words
+#'@param x Character vector to be converted to camel-case
+#'@return A Character vector with underscore separated words
 #'@export
-
-fillUnderscore<-Vectorize(function(x){
+fillUnderscore<-function(x){
+  under<-function(x){
   whichCaps<-which(unlist(strsplit(x,""))!=unlist(strsplit(tolower(x),"")))
   if(1 %in% whichCaps){whichCaps<-whichCaps[-1]}
   xSplit<-substring(x,c(1,whichCaps),c(whichCaps-1,nchar(x)))
   filled<-paste(tolower(xSplit),collapse="_")
   return(filled)
-})
+  }
+  return(unlist(lapply(x,under)))
+}
 
 #'Convert string of separated words to camel-case
 #'
-#'@param x Character(vector) to be converted to camel-case
+#'@param x Character vector to be converted to camel-case
 #'@param sep Character separating words in the original string
 #'@return Camel-case string(s)
 #'@export
-
-camelCase<-Vectorize(function(x,sep="_"){
-  xSplit<-unlist(strsplit(x,sep))
+camelCase<-function(x,sep="_"){
+  camel<-function(toCamel,sep){
+  xSplit<-unlist(strsplit(toCamel,sep))
   
   if(length(xSplit)==1){return(xSplit)}
   for(n in 2:length(xSplit)){
@@ -29,4 +31,6 @@ camelCase<-Vectorize(function(x,sep="_"){
   }
   camelled<-paste(xSplit,collapse="")
   return(camelled)
-})
+  }
+  return(unlist(lapply(x,camel,sep=sep)))  
+}
